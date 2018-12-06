c959970d46fd227161a84271826f9e9cbf7bf81d
Basile Nouvellet <basile.nouvellet@ponts.org>
2018-12-06 14:40:14 +0800

    Improve log messages in server

--- server/index.js
+++ server/index.js
@@ -8,20 +8,22 @@ const app = express();
 const port = process.env.port || 8080;
 
 // database
-const db = mysql.createConnection({
+const dbOptions = {
   host: 'birdie-test.cyosireearno.eu-west-2.rds.amazonaws.com',
   port: 3306,
   user: 'test-read',
   password: 'xnxPp6QfZbCYkY8',
   database: 'birdietest',
-});
+};
+
+const db = mysql.createConnection(dbOptions);
 
 const table = 'census_learn_sql';
 
 // connect to database
 db.connect((err) => {
   if (err) throw err;
-  console.log('Connected to database');
+  console.log(`Connected to database '${dbOptions.database}'`);
 });
 
 // routes
@@ -36,22 +38,21 @@ app.get('/data', (req, res, next) => {
       'ORDER BY average_age DESC',
     ].join(' ');
 
-    console.log('SELECT', variable);
+    console.log(`New SQL query: SELECT \`${variable}\` [...]`);
 
     db.query(sqlQuery, (error, results) => {
       if (error) throw error;
       res.send(results);
     });
   } else {
-    console.log('Empty variable in SQL request', variable);
-    next();
+    throw new Error(`Empty variable in SQL request: ${variable}`);
   }
 });
 
 app.get('/columns', (req, res) => {
-  const sqlQuery = `SHOW columns from ${table}`;
+  const sqlQuery = `SHOW columns from \`${table}\``;
 
-  console.log(sqlQuery);
+  console.log(`New SQL query: ${sqlQuery}`);
 
   db.query(sqlQuery, (error, results) => {
     if (error) throw error;
@@ -59,13 +60,7 @@ app.get('/columns', (req, res) => {
   });
 });
 
-// error handler
-app.use((err, req, res, next) => { // eslint-disable-line no-unused-vars
-  console.error(err.stack);
-  res.status(500).send('Something broke!');
-});
-
 // launch server
 app.listen(port, () => {
-  console.log(`Server running on port ${port}!`);
+  console.log(`Server running on port ${port}`);
 });
