e7cfeb11302d00ff42488e35abab276c8aa4fd84
Basile Nouvellet <basile.nouvellet@ponts.org>
2018-12-06 15:09:47 +0800

    Add catchall route in server

--- server/index.js
+++ server/index.js
@@ -64,6 +64,12 @@ app.get('/columns', (req, res) => {
   });
 });
 
+// "catchall" handler:
+// for any request that doesn't match one above, send back React's index.html file
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build/index.html'));
+});
+
 // launch server
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
